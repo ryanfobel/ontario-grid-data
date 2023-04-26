c3ba2d6b895efdf5d25dc44fe3bb2c1ea7a44f2d
Ryan Fobel <ryan@fobel.net>
2023-04-26 01:29:34 -0400

    Handle empty json list

--- pipelines/ieso/__main__.py
+++ pipelines/ieso/__main__.py
@@ -12,18 +12,20 @@ CLEAN_DATA_PATH = os.path.join(ROOT, "..", "..", "data", "clean", "ieso.ca")
 def main():
     now = dt.datetime.utcnow()
     output_json = fetch_production(target_datetime=now)
-    output_json[-1]['datetime'] = output_json[-1]['datetime'].isoformat()
-    output_path = os.path.join(CLEAN_DATA_PATH, "latest", "output.json")
-    print(output_path)
-    with open(output_path, "w") as f:
-        f.write(json.dumps(output_json[-1], indent=4))
+    if output_json:
+        output_json[-1]['datetime'] = output_json[-1]['datetime'].isoformat()
+        output_path = os.path.join(CLEAN_DATA_PATH, "latest", "output.json")
+        print(output_path)
+        with open(output_path, "w") as f:
+            f.write(json.dumps(output_json[-1], indent=4))
 
     price_json = fetch_price(target_datetime=now)
-    price_json[-1]['datetime'] = price_json[-1]['datetime'].isoformat()
-    price_path = os.path.join(CLEAN_DATA_PATH, "latest", "price.json")
-    print(price_path)
-    with open(price_path, "w") as f:
-        f.write(json.dumps(price_json[-1], indent=4))
+    if price_json:
+        price_json[-1]['datetime'] = price_json[-1]['datetime'].isoformat()
+        price_path = os.path.join(CLEAN_DATA_PATH, "latest", "price.json")
+        print(price_path)
+        with open(price_path, "w") as f:
+            f.write(json.dumps(price_json[-1], indent=4))
 
 if __name__ == "__main__":
     main()
