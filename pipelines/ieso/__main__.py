4c9da5b41ee7f922651a913a93cd74bbe8d41bed
Ryan Fobel <ryan@fobel.net>
2023-04-26 00:38:21 -0400

    Add ieso price

--- pipelines/ieso/__main__.py
+++ pipelines/ieso/__main__.py
@@ -2,7 +2,7 @@ import os
 import datetime as dt
 import json
 
-from .CA import fetch_production
+from .CA import fetch_production, fetch_price
 
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -11,13 +11,19 @@ CLEAN_DATA_PATH = os.path.join(ROOT, "..", "..", "data", "clean", "ieso.ca")
 
 def main():
     now = dt.datetime.utcnow()
-    data_json = fetch_production(target_datetime=now)
-    data_json[-1]['datetime'] = data_json[-1]['datetime'].isoformat()
+    output_json = fetch_production(target_datetime=now)
+    output_json[-1]['datetime'] = output_json[-1]['datetime'].isoformat()
     output_path = os.path.join(CLEAN_DATA_PATH, "latest", "output.json")
     print(output_path)
     with open(output_path, "w") as f:
-        f.write(json.dumps(data_json[-1], indent=4))
+        f.write(json.dumps(output_json[-1], indent=4))
 
+    price_json = fetch_price(target_datetime=now)
+    price_json[-1]['datetime'] = price_json[-1]['datetime'].isoformat()
+    price_path = os.path.join(CLEAN_DATA_PATH, "latest", "price.json")
+    print(price_path)
+    with open(price_path, "w") as f:
+        f.write(json.dumps(price_json[-1], indent=4))
 
 if __name__ == "__main__":
     main()
