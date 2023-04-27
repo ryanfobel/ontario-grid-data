0b3bdbd7b1d6bb9b8b37c0eaaf223e3f08da77f1
Ryan Fobel <ryan@fobel.net>
2023-04-27 09:49:02 -0400

    Breakout co2signal_get_latest function

--- pipelines/co2signal.py
+++ pipelines/co2signal.py
@@ -13,21 +13,23 @@ ROOT = os.path.dirname(os.path.abspath(__file__))
 CLEAN_DATA_PATH = os.path.join(ROOT, "..", "data", "clean", "co2signal.com")
 
 
+def co2signal_get_latest(token: str, country_code: str="CA-ON") -> dict:
+    url = f"https://api.co2signal.com/v1/latest?countryCode={country_code}"
+    response = requests.get(url, headers={'auth-token': f'{token}'})
+    if response.status_code != 200:
+        raise RuntimeError(response.status_code)
+    json_string = json.dumps(json.loads(response.content), indent=4)
+    print(json_string)
+    with open(os.path.join(CLEAN_DATA_PATH, country_code, "latest.json"), "w") as f:
+            f.write(json_string)
+    return json.loads(json_string)
+
+
 def main():
     load_dotenv()
     print("query co2signal...")
     if "CO2SIGNAL_API_TOKEN" in os.environ.keys():
-        def co2_signal_get_latest(token: str, country_code: str="CA-ON"):
-            url = f"https://api.co2signal.com/v1/latest?countryCode={country_code}"
-            response = requests.get(url, headers={'auth-token': f'{token}'})
-            if response.status_code != 200:
-                raise RuntimeError(response.status_code)
-            json_string = json.dumps(json.loads(response.content), indent=4)
-            print(json_string)
-            with open(os.path.join(CLEAN_DATA_PATH, country_code, "latest.json"), "w") as f:
-                 f.write(json_string)
- 
-        co2_signal_get_latest(os.environ["CO2SIGNAL_API_TOKEN"])
+        co2signal_get_latest(os.environ["CO2SIGNAL_API_TOKEN"])
     
     filepath = os.path.abspath(os.path.join(CLEAN_DATA_PATH, "CA-ON", "latest.json"))
     hourly_path = os.path.abspath(os.path.join(CLEAN_DATA_PATH, "CA-ON", "hourly.csv"))
