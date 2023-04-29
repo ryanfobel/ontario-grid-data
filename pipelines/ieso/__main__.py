7cac03d0144e62314f2532c27be215a15e4c2097
Ryan Fobel <ryan@fobel.net>
2023-04-29 15:19:47 -0400

    Refactor ieso feed

--- pipelines/ieso/__main__.py
+++ pipelines/ieso/__main__.py
@@ -12,39 +12,43 @@ CLEAN_DATA_PATH = os.path.join(ROOT, "..", "..", "data", "clean", "ieso.ca")
 
 
 def main():
+    TZ = "America/Toronto"
     now = dt.datetime.utcnow()
-    summary_json = fetch_production_by_fuel(target_datetime=now)
-    if summary_json:
-        summary_json[-1]['datetime'] = pd.to_datetime(
-            summary_json[-1]['datetime']
-        ).tz_convert("America/Toronto").isoformat()
-        summary_path = os.path.join(CLEAN_DATA_PATH, "latest", "summary.json")
-        print(summary_path)
-        with open(summary_path, "w") as f:
-            f.write(json.dumps(summary_json[-1], indent=4))
+    production_by_source = fetch_production_by_fuel(target_datetime=now)
+    if production_by_source:
+        production_by_source[-1]['datetime'] = pd.to_datetime(
+            production_by_source[-1]['datetime']
+        ).tz_convert(TZ).isoformat()
+        with open(os.path.join(CLEAN_DATA_PATH, "latest", "production_by_source.json"), "w") as f:
+            f.write(json.dumps(production_by_source[-1], indent=4))
 
-    data = fetch_production(target_datetime=now)
-    if len(data):
-        timestamp = data["dt"].unique()[-1]
-        data = data[data["dt"] == timestamp]
+    production_data = fetch_production(target_datetime=now)
+    if len(production_data):
+        timestamp = production_data["dt"].unique()[-1].tz_convert(TZ)
+        production_data = production_data[production_data["dt"] == timestamp]
 
-        plants = data[["name", "fuel"]].set_index("name").to_dict()["fuel"]
+        plants = {
+            "datetime": timestamp.isoformat(),
+            "plants": production_data[["name", "fuel"]].set_index("name").to_dict()["fuel"]
+        }
         with open(os.path.join(CLEAN_DATA_PATH, "latest", "plants.json"), "w") as f:
             f.write(json.dumps(plants, indent=4))
 
-        output = data[["name", "production"]].set_index("name").to_dict()["production"]
-        with open(os.path.join(CLEAN_DATA_PATH, "latest", "output.json"), "w") as f:
+        production_by_plant = production_data[["name", "production"]].set_index("name").to_dict()["production"]
+        output = {
+            "datetime": timestamp.isoformat(),
+            "production_by_plant": production_by_plant
+        }
+        with open(os.path.join(CLEAN_DATA_PATH, "latest", "production_by_plant.json"), "w") as f:
             f.write(json.dumps(output, indent=4))
 
-    price_json = fetch_price(target_datetime=now)
-    if price_json:
-        price_json[-1]['datetime'] = pd.to_datetime(
-            price_json[-1]['datetime']
+    price = fetch_price(target_datetime=now)
+    if price:
+        price[-1]['datetime'] = pd.to_datetime(
+            price[-1]['datetime']
         ).tz_convert("America/Toronto").isoformat()
-        price_path = os.path.join(CLEAN_DATA_PATH, "latest", "price.json")
-        print(price_path)
-        with open(price_path, "w") as f:
-            f.write(json.dumps(price_json[-1], indent=4))
+        with open(os.path.join(CLEAN_DATA_PATH, "latest", "price.json"), "w") as f:
+            f.write(json.dumps(price[-1], indent=4))
 
 if __name__ == "__main__":
     main()
