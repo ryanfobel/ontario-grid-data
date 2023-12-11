e13ef278b7863734a488bd761bedfe60d35c3251
Ryan Fobel <ryan@fobel.net>
2023-12-11 18:02:11 -0500

    Fix oeb pipeline

--- pipelines/oeb.py
+++ pipelines/oeb.py
@@ -13,6 +13,13 @@ ELECTRICITY_RATES_FILEPATH = os.path.abspath(os.path.join(RAW_DATA_PATH, "histor
 GAS_RATES_FILEPATH = os.path.abspath(os.path.join(RAW_DATA_PATH, "historcial-natural-gas-rates.html"))
 
 
+def make_subs(headers, subs):
+    result = headers
+    for a, b in subs.items():
+        result = [s.replace(a, b).strip() for s in result]
+    return result
+
+
 def get_electricity_rates():
     response = requests.get("https://www.oeb.ca/consumer-information-and-protection/electricity-rates/historical-electricity-rates")
     with open(ELECTRICITY_RATES_FILEPATH, "w") as f:
@@ -25,45 +32,77 @@ def get_gas_rates():
         f.write(response.content.decode("utf-8"))
 
 
-def convert_electricity_table_to_df(table):
+def convert_table_to_df(table):
     rows = table.find_all(name="tr")
-    headers = [th.text.replace("\xa0", " ") for th in rows[0].find_all("th")]
-    rows = [[td.text.replace("\n", "").replace("*", "") for td in tr.find_all(name="td")] for tr in rows[1:]]
-    df = pd.DataFrame(rows, columns=headers).set_index("Effective date")
+    headers = make_subs(
+        [th.text for th in rows[0].find_all("th")],
+        subs = {
+            "\xa0": " ",
+            "\n": "",
+            "   ": " ",
+            "*": "",
+        }
+    )
+
+    # pad missing columns
+    rows = rows[1:]
+    for i, tr in enumerate(rows):
+        tds = [td.text for td in tr.find_all(name="td")]
+        rows[i] = tds + (len(headers) - len(tds))*['']
+
+    data = {}
+    for i, col in enumerate(headers):
+        data[col] = make_subs(
+            [row[i] for row in rows],
+            subs = {
+                "*": "",
+                ",": "",
+            }
+        )
+    return pd.DataFrame(data)
+
+
+def convert_electricity_table_to_df(table):
+    df = convert_table_to_df(table)
+    df = df.set_index("Effective date")
     df.index = pd.to_datetime(df.index)
     for col in df.columns:
         df[col] = (
             df[col].str.replace(" ¢ per kWh", "")
-            .str.replace("\xa0", " ")
         )
     if "Residential threshold for lower tier price (kWh per month)" in df.columns:
-        pat = r"(?P<summer>\S+) \(Summer\)\t\t\t(?P<winter>\S+) \(Winter\)"
+        pat = r"(?P<summer>\S+) \(Summer\)\s+(?P<winter>\S+) \(Winter\)"
         special = df['Residential threshold for lower tier price (kWh per month)'].str.extract(pat).dropna()
         df["Residential threshold for lower tier price (kWh per month) [Summer]"] = df["Residential threshold for lower tier price (kWh per month)"]
         df["Residential threshold for lower tier price (kWh per month) [Winter]"] = df["Residential threshold for lower tier price (kWh per month)"]
         df = df.drop(columns=["Residential threshold for lower tier price (kWh per month)"])
         df.loc[special.index, "Residential threshold for lower tier price (kWh per month) [Summer]"] = special["summer"]
         df.loc[special.index, "Residential threshold for lower tier price (kWh per month) [Winter]"] = special["winter"]
-        na_index = df[df["Higher tier price (¢ per kWh)"].isna()].index
+        na_index = df[df["Higher tier price (¢ per kWh)"] == ""].index
+        df.loc[na_index, "Residential threshold for lower tier price (kWh per month) [Summer]"] = 0
+        df.loc[na_index, "Residential threshold for lower tier price (kWh per month) [Winter]"] = 0
         df.loc[na_index, "Higher tier price (¢ per kWh)"] = df.loc[na_index, "Lower tier price (¢ per kWh)"]
 
     for col in df.columns:
-        df[col] = df[col].str.replace(",", "").astype(float)
+        df[col] = df[col].astype(float)
 
     return df
 
 
 def convert_gas_table_to_df(table):
-    rows = table.find_all(name="tr")
-    headers = [th.text.replace("\xa0", " ").replace("\n", "") for th in rows[0].find_all("th")]
-    rows = [[td.text.replace("\n", "").replace("*", "") for td in tr.find_all(name="td")] for tr in rows[1:]]
-    df = pd.DataFrame(rows, columns=headers).set_index("Date")
+    df = convert_table_to_df(table)
+    df = df.set_index("Date")
+    df = df.drop(columns="Detailed rates")
     df.index = pd.to_datetime(df.index)
-    for col in df.columns:
-        df[col] = (
-            df[col].str.replace(",", "").replace("-", "").replace(" ", "").
-            replace("\xa0", "").replace("", "nan").astype(float)
-        )
+
+    if "Gas cost adjustment (¢/m³)" in df.columns:
+        na_index = df[df["Gas cost adjustment (¢/m³)"] == "-"].index
+        df.loc[na_index, "Effective price (¢/m³)"] = df.loc[na_index, "Commodity price (¢/m³)"] 
+        df.loc[na_index, "Gas cost adjustment (¢/m³)"] = 0
+
+    # for col in df.columns:
+    #     df[col] = df[col].astype(float)
+
     return df
 
 
@@ -72,7 +111,7 @@ def main():
     try:
         with open(ELECTRICITY_RATES_FILEPATH, "r") as f:
             soup = BeautifulSoup(f.read(), "html.parser")
-        rate_types = [h2.contents[1] for h2 in soup.find_all(name="h2") if len(h2.contents) > 1]
+        rate_types = [h2.contents[0] for h2 in soup.find_all(name="h2") if len(h2.contents) > 1]
         tables = soup.find_all(name="table")
         for rate_type, table in zip(rate_types, tables):
             df = convert_electricity_table_to_df(table)
@@ -84,7 +123,7 @@ def main():
     try:
         with open(GAS_RATES_FILEPATH, "r") as f:
             soup = BeautifulSoup(f.read(), "html.parser")
-        zones = [h2.contents[1] for h2 in soup.find_all(name="h2") if len(h2.contents) > 1]
+        zones = [h2.contents[0] for h2 in soup.find_all(name="h2") if len(h2.contents) > 1]
         tables = soup.find_all(name="table")
         for zone, table in zip(zones, tables):
             df = convert_gas_table_to_df(table)
