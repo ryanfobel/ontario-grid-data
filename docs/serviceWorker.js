0f79c045cbcdbd338925e430aba8679b492af3e6
Ryan Fobel <ryan@fobel.net>
2024-05-28 19:57:46 -0400

    Update dashboard

--- docs/serviceWorker.js
+++ docs/serviceWorker.js
@@ -1,5 +1,5 @@
 const appName = 'Ontario grid data'
-const appCacheName = 'Ontario grid data-52cbfe65e4774bcb9668dc4446724ff1';
+const appCacheName = 'Ontario grid data-c7ba81d90f9b4c8c8a58d73ed1b5f832';
 
 const preCacheFiles = ['images/favicon.ico', 'images/icon-vector.svg', 'images/icon-32x32.png', 'images/icon-192x192.png', 'images/icon-512x512.png', 'images/apple-touch-icon.png', 'images/index_background.png'];
 
