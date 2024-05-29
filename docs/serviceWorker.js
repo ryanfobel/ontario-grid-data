24271d08dda0b50211a4529712aa0568e8f0140f
Ryan Fobel <ryan@fobel.net>
2024-05-29 09:40:49 -0400

    Try fix for iphone

--- docs/serviceWorker.js
+++ docs/serviceWorker.js
@@ -1,5 +1,5 @@
 const appName = 'Ontario grid data'
-const appCacheName = 'Ontario grid data-d2ad295c91a44e36bc0d83ff178a9d41';
+const appCacheName = 'Ontario grid data-8290940c6b024a828e44462aca2aba73';
 
 const preCacheFiles = ['images/favicon.ico', 'images/icon-vector.svg', 'images/icon-32x32.png', 'images/icon-192x192.png', 'images/icon-512x512.png', 'images/apple-touch-icon.png', 'images/index_background.png'];
 
