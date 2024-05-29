dc4201a075fb0ff9bb3951ff3c389416417b3134
Ryan Fobel <ryan@fobel.net>
2024-05-29 12:08:33 -0400

    Reduce data to 90d and fix md cell

--- docs/serviceWorker.js
+++ docs/serviceWorker.js
@@ -1,5 +1,5 @@
 const appName = 'Ontario grid data'
-const appCacheName = 'Ontario grid data-012e7a5649ff4334a1ff9f8007d03b24';
+const appCacheName = 'Ontario grid data-4d1821088fce462cab9d7b99d35d4f23';
 
 const preCacheFiles = ['images/favicon.ico', 'images/icon-vector.svg', 'images/icon-32x32.png', 'images/icon-192x192.png', 'images/icon-512x512.png', 'images/apple-touch-icon.png', 'images/index_background.png'];
 
